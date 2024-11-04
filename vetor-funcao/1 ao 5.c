082f01776bf377464dc3298e7c45c7067fd55721
Brendha Casaro <brendhameira9@gmail.com>
2024-11-03 23:55:25 -0300

    novo

---
+++ vetor-funcao/1 ao 5.c
@@ -0,0 +1,16 @@
+#include "stdbrendha.h"
+#include <locale.h>
+
+/*Crie o programa dividido em funções:
+a)	Função Armazena: é utilizada para guardar dez números quaisquer no vetor A.
+b)	Função Vetor_B: construa o vetor B de mesma dimensão com os elementos do vetor A baseado nas seguintes condições: se o índice for par soma dois ao conteúdo do vetor, caso o índice seja ímpar some três ao conteúdo do vetor A.
+c)	Exiba: exiba no interior desta função o conteúdo dos dois vetores.
+*/
+
+int main () 
+{
+    void armazena()
+    {
+        
+    }
+}
